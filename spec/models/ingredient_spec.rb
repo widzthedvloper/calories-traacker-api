e7e0a33e4935da950fe1271d2568af63894bcc51
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-09-13 07:42:04 -0400

    Add test for ingredient model

--- spec/models/ingredient_spec.rb
+++ spec/models/ingredient_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Ingredient, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # each record belongs to a food
+  it { should belong_to(:food) }
+
+  # the name of the ingredient and the number of calories should be present
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:calorie) }
 end
