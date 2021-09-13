60166a9a3f3ab56f8e4f4fc2ce14ca4b78a21f18
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-09-13 07:36:58 -0400

    Add test for food model

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Food, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # the model should have a 1 to many relationship
+  it { should have_many(:ingredients).dependent(:destroy) }
+
+  # ensure the name and the author of the food is present
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:created_by) }
 end
