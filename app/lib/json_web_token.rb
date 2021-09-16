758863133a434946b38a35d3eac140b1197688be
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-09-16 12:16:18 -0400

    Fixing errors

--- app/lib/json_web_token.rb
+++ app/lib/json_web_token.rb
@@ -26,18 +26,6 @@ module ExceptionHandler
 
   class InvalidToken < StandardError; end
 
-  included do
-    # Define custom handlers
-    rescue_from ActiveRecord::RecordInvalid, with: :four_twenty_two
-    rescue_from ExceptionHandler::AuthenticationError, with: :unauthorized_request
-    rescue_from ExceptionHandler::MissingToken, with: :four_twenty_two
-    rescue_from ExceptionHandler::InvalidToken, with: :four_twenty_two
-
-    rescue_from ActiveRecord::RecordNotFound do |e|
-      json_response({ message: e.message }, :not_found)
-    end
-  end
-
   private
 
   # JSON response with message; Status code 422 - unprocessable entity
--- spec/auth/authorize_api_request_spec.rb
+++ spec/auth/authorize_api_request_spec.rb
@@ -1,8 +1,35 @@
 # rubocop:disable Metrics/BlockLength
 require 'rails_helper'
 
-require '../auth/support/controller_spec_helper'
-require '../auth/support/reqest_spec_helper'
+# generate tokens from user id
+def token_generator(user_id)
+  JsonWebToken.encode(user_id: user_id)
+end
+
+# generate expired tokens from user id
+def expired_token_generator(user_id)
+  JsonWebToken.encode({ user_id: user_id }, (Time.now.to_i - 10))
+end
+
+# return valid headers
+def valid_headers
+  {
+    'Authorization' => token_generator(user.id),
+    'Content-Type' => 'application/json'
+  }
+end
+
+# return invalid headers
+def invalid_headers
+  {
+    'Authorization' => nil,
+    'Content-Type' => 'application/json'
+  }
+end
+
+def json
+  JSON.parse(response.body)
+end
 
 RSpec.describe AuthorizeApiRequest do
   # Create test user
