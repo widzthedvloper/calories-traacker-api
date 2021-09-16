8ef6d97928b8dfe90e079f167f70fdbb8a65766a
Widzmarc Jean Nesly Phelle <widzmarc@gmail.com>
2021-09-16 10:35:32 -0400

    Fix typo

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it { should have_may(:foods) }
+  it { should have_many(:foods) }
 
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:email) }
